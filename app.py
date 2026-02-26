2b1d02041fc18e8b16f4e14b511944cc5b8be17f
Charlesisthebest476 <charlesisthebest2@gmail.com>
2026-02-26 14:18:41 -0500

    Set up basic Flask application with index route

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 from google.genai import types
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # 'index.html' refers to the file inside the /templates folder
+    return render_template("index.html", title="Flask is working!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
