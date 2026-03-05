4eaf14bdc71180ab4a7732f9aebfd4619508f6ac
Charlesisthebest476 <charlesisthebest2@gmail.com>
2026-03-04 23:58:34 -0500

    My first commit

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from google import genai
 from google.genai import types
 app = Flask(__name__)
 
-client = genai.Client(api_key = GEMINI_API_KEY)
+client = genai.Client(api_key = "AIzaSyB36C1zyAp97-SL0RRtbvWJLWeW1rWffR8")
 
 
 @app.route("/")
 def index():
     # 'index.html' refers to the file inside the /templates folder
-    return render_template("index.html", title="Flask is working!")
+    return render_template("index.html", result="Flask is working!")
 
 if __name__ == "__main__":
     app.run(debug=True)
